05dd6aadb1ca59eeaa4e9e512754f94459072749
Kimberly <khb1421@jagmail.southalabama.edu>
2017-09-25 16:53:35 -0500

    first commit

---
+++ app/src/main/java/edu/southalabama/csc331/csc331_fa17_mbmgames/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.southalabama.csc331.csc331_fa17_mbmgames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
