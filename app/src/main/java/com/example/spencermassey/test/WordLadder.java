6ad995644b63f7d5adf670a593a5e63a51a2c47f
sol_s <bjmcallister333@gmail.com>
2017-10-16 12:33:25 -0500

    NEW

--- app/src/main/java/com/example/spencermassey/test/WordLadder.java
+++ app/src/main/java/com/example/spencermassey/test/WordLadder.java
@@ -15,15 +15,9 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.lang.String;
 
 
 public class WordLadder extends AppCompatActivity {
-    public String hint = "A person who has authority over others";
-    public String hint1 = "The rendered fat of hogs";
-    public String hint2 = "Any part of the earth's surface not covered by water";
-    public String hint3 = "Part of a highway large enough for one vehicle";
-    public String hint4 = "A stick or short staff used to assist one in walking";
 
     public int count = 0;
 
@@ -108,6 +102,7 @@ public class WordLadder extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((userInput1.getText().toString().equals("lard"))) {
                     userInput1.setTextColor(Color.GREEN);
+                    userInput1.setFocusable(false);
                     count++;
                 }
                 IBinder bob = getWindow().getDecorView().getRootView().getWindowToken();
@@ -124,6 +119,7 @@ public class WordLadder extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((userInput2.getText().toString().equals("land"))) {
                     userInput2.setTextColor(Color.GREEN);
+                    userInput2.setFocusable(false);
                     count++;
                 }
                 IBinder bob = getWindow().getDecorView().getRootView().getWindowToken();
@@ -140,6 +136,7 @@ public class WordLadder extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((userInput3.getText().toString().equals("lane"))) {
                     userInput3.setTextColor(Color.GREEN);
+                    userInput3.setFocusable(false);
                     count++;
                 }
                 IBinder bob = getWindow().getDecorView().getRootView().getWindowToken();
@@ -156,6 +153,7 @@ public class WordLadder extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((userInput4.getText().toString().equals("cane"))) {
                     userInput4.setTextColor(Color.GREEN);
+                    userInput4.setFocusable(false);
                     count++;
                 }
                 IBinder bob = getWindow().getDecorView().getRootView().getWindowToken();
@@ -164,77 +162,6 @@ public class WordLadder extends AppCompatActivity {
                 return true;
             }
         });
-//        ((EditText) findViewById(R.id.Answer)).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                EditText userInput = (EditText) findViewById(R.id.Answer);
-//                if (!hasFocus) {
-//                    if ((userInput.getText().toString().equals("lord"))) {
-//                        userInput.setTextColor(Color.GREEN);
-//                        count++;
-//                    }
-//                }
-//            }
-//        });
-
-
-
-            ((EditText) findViewById(R.id.Answer1)).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-                    EditText userInput = (EditText) findViewById(R.id.Answer1);
-                    if (!hasFocus) {
-                        if ((userInput.getText().toString().equals("lard"))) {
-                            userInput.setTextColor(Color.GREEN);
-                            count++;
-                        }
-                    }
-                }
-            });
-            ((EditText) findViewById(R.id.Answer2)).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-
-                    EditText userInput = (EditText) findViewById(R.id.Answer2);
-                    if (!hasFocus) {
-                        if ((userInput.getText().toString().equals("land"))) {
-                            userInput.setTextColor(Color.GREEN);
-                            count++;
-                        }
-                    }
-                }
-            });
-            ((EditText) findViewById(R.id.Answer3)).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-
-                    EditText userInput = (EditText) findViewById(R.id.Answer3);
-                    if (!hasFocus) {
-                        if ((userInput.getText().toString().equals("lane"))) {
-                            userInput.setTextColor(Color.GREEN);
-                            count++;
-                        }
-                    }
-                }
-            });
-            ((EditText) findViewById(R.id.Answer4)).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-                    EditText userInput = (EditText) findViewById(R.id.Answer4);
-                    if (!hasFocus) {
-                        if ((userInput.getText().toString().equals("cane"))) {
-                            userInput.setTextColor(Color.GREEN);
-                            count++;
-                        }
-                    }
-                }
-            });
-
     }
     public void Restart(){
         this.recreate();
@@ -261,50 +188,31 @@ public class WordLadder extends AppCompatActivity {
     public void display(View v)
     {
         TextView tv= (TextView) findViewById(R.id.hint);
-        //alter text of textview widget
-        tv.setText(hint);
+        tv.setText(R.string.hints);
 
-        //assign the textview forecolor
-        //tv.setTextColor(Color.GREEN);
     }
     public void display1(View v)
     {
         TextView tv= (TextView) findViewById(R.id.hint1);
-        //alter text of textview widget
-        tv.setText(hint1);
-
-        //assign the textview forecolor
-        //tv.setTextColor(Color.GREEN);
+        tv.setText(R.string.hint1);
     }
 
 
     public void display2(View v)
     {
         TextView tv= (TextView) findViewById(R.id.hint2);
-        //alter text of textview widget
-        tv.setText(hint2);
-
-        //assign the textview forecolor
-        //tv.setTextColor(Color.GREEN);
+        tv.setText(R.string.hint2);
     }
 
     public void display3(View v)
     {
         TextView tv= (TextView) findViewById(R.id.hint3);
-        //alter text of textview widget
-        tv.setText(hint3);
-
-        //assign the textview forecolor
-        //tv.setTextColor(Color.GREEN);
+        tv.setText(R.string.hint3);
     }
     public void display4(View v)
     {
         TextView tv= (TextView) findViewById(R.id.hint4);
-        //alter text of textview widget
-        tv.setText(hint4);
-
-        //assign the textview forecolor
-        //tv.setTextColor(Color.GREEN);
+        tv.setText(R.string.hint4);
     }
 }
 
