6d0beee879987b96479d8ca11a1b7093d77daf7c
khb1421 <31966747+khb1421@users.noreply.github.com>
2017-11-29 09:43:52 -0600

    This works.
    The database file didn't have the correct metadata, so the checks were failing.  The checks now work correctly.

--- app/src/main/java/com/example/spencermassey/test/DBHelper.java
+++ app/src/main/java/com/example/spencermassey/test/DBHelper.java
@@ -2,67 +2,60 @@ package com.example.spencermassey.test;
 
 //import java.util.ArrayList;
 //import java.util.List;
-
+// database integration help from https://www.androidhive.info/2011/11/android-sqlite-database-tutorial/ and https://blog.reigndesign.com/blog/using-your-own-sqlite-database-in-android-applications/
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class DBHelper extends SQLiteOpenHelper {
 
     // All Static variables
     // Database Version
     private static final int DATABASE_VERSION = 1;
-
+    private static final String DATABASE_PATH = "/data/data/com.example.spencermassey.test/databases/";
     // Database Name
-    private static final String DATABASE_NAME = "word_new2.db";
+    private static final String DATABASE_NAME = "words.db";
 
     // table name
-    private static final String TABLE_WORDS = "words";
-
+    private static final String TABLE_WORDS = "word_db";
+    //private SQLiteDatabase myDataBase;
     // Column names
-    private static final String COL_ID = "id";
+    private static final String COL_ID = "_id";
     private static final String COL_WORD = "word";
     private static final String COL_PHRASE = "phrase";
-    private static final String COL_SET = "length";
+    private static final String COL_SET = "set";
+    private SQLiteDatabase myData;
+    private final Context myContext;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.myContext = context;
     }
 
     // Creating Tables
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String CREATE_WORD_4_TABLE = "CREATE TABLE " + TABLE_WORDS + "("
-                + COL_ID + " INTEGER PRIMARY KEY," + COL_WORD + " TEXT,"
-                + COL_PHRASE+ " TEXT," + COL_SET + " TEXT" + ")";
-        db.execSQL(CREATE_WORD_4_TABLE);
-    }
-
-    // Upgrading database
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_WORDS);
-
-        // Create tables again
-        onCreate(db);
-    }
-
-
-    // Adding new word
-    void addWord(Word word) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COL_WORD, word.getWord()); // word
-        values.put(COL_PHRASE, word.getPhrase()); // phrase
-        values.put(COL_SET, word.getSet()); // set
-
-        // Inserting Row
-        db.insert(TABLE_WORDS, null, values);
-        db.close(); // Closing database connection
+    public void createDataBase() throws IOException {
+        boolean dbExists = checkDatabase();
+        if(dbExists)
+        {
+            //we have a database
+        }
+        else{
+            this.getReadableDatabase();
+            try{
+                copyDatabase();
+            } catch (IOException e) {
+                throw new Error("Can't copy database");
+            };
+        }
     }
 
     // Get single word
@@ -82,7 +75,6 @@ public class DBHelper extends SQLiteOpenHelper {
 
     // Getting All Words
     public Cursor getAllWords() {
-        //List<Word> wordList = new ArrayList<Word>();
         // Select All Query
         String selectQuery = "SELECT  * FROM " + TABLE_WORDS;
 
@@ -91,28 +83,6 @@ public class DBHelper extends SQLiteOpenHelper {
 
         return cursor;
     }
-
-    // Updating single word
-    public int updateWord(Word word) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COL_WORD, word.getWord());
-        values.put(COL_PHRASE, word.getPhrase());
-
-        // updating row
-        return db.update(TABLE_WORDS, values, COL_ID + " = ?",
-                new String[] { String.valueOf(word.getID()) });
-    }
-
-    // Deleting single word
-    public void deleteWord(Word word) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_WORDS, COL_ID + " = ?",
-                new String[] { String.valueOf(word.getID()) });
-        db.close();
-    }
-
 
     // Getting word Count
     public int getWordCount() {
@@ -125,4 +95,57 @@ public class DBHelper extends SQLiteOpenHelper {
         return cursor.getCount();
     }
 
+    private boolean checkDatabase(){
+        SQLiteDatabase checkDB = null;
+        try{
+            String myPath = DATABASE_PATH + DATABASE_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        }catch (SQLiteException e){
+
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null ? true : false;
+    }
+
+    private void copyDatabase() throws IOException{
+        InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+        String outFileName = DATABASE_PATH + DATABASE_NAME;
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void openDataBase() throws SQLiteException{
+        String myPath = DATABASE_PATH + DATABASE_NAME;
+        myData = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    @Override
+    public synchronized void close() {
+
+        if(myData != null)
+            myData.close();
+
+        super.close();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //blank, our database is preloaded
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //blank, our database is preloaded
+    }
 }
--- app/src/main/java/com/example/spencermassey/test/MainActivity.java
+++ app/src/main/java/com/example/spencermassey/test/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.spencermassey.test;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper myDBHelper = new DBHelper(this);
+        myDBHelper = new DBHelper(this);
+        try{
+            myDBHelper.createDataBase();
+        }catch (IOException ioe){
+            throw new Error("Can't create db.");
+        }
+        try{
+            myDBHelper.openDataBase();
+        }catch (SQLiteException sqle){
+            throw sqle;
+        }
     }
     //Switches to Word Jumble
     public void GoToWordJumble(View view)
